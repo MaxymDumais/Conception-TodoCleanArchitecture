0272c988cfc1b8cd5c2a12971ad2fc00ba3464ad
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-17 10:03:13 -0400

    Creation of the AuthController.cs and a route test

---
+++ CleanTodo.WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using CleanTodo.Application.UseCase;
+using CleanTodo.Application.UseCases.Todo;
+using CleanTodo.Domain.DTOS;
+using CleanTodo.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanTodo.API.Controllers
+{
+   [ApiController]
+   [Route("api/[controller]")]
+   public class AuthController : ControllerBase
+   {
+      [Route("test")]
+      [HttpGet]
+      public async Task<IActionResult> GetAll()
+      {
+         return Ok("ok");
+      }
+   }
+}
