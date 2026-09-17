09c1d108897791c5ee673457a42fe9e1e3368e63
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-17 10:41:56 -0400

    Creating the useCases for Register and Login, and the validations for the register

---
+++ CleanTodo.Application/UseCases/User/LoginUserUseCase.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanTodo.Application.UseCases.User
+{
+   internal class LoginUserUseCase
+   {
+   }
+}
---
+++ CleanTodo.Application/Validators/RegisterUserValidation.cs
@@ -0,0 +1,28 @@
+﻿using CleanTodo.Domain.DTOS;
+using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanTodo.Application.Validators
+{
+   public class RegisterUserValidation : AbstractValidator<UserDto>
+   {
+      public RegisterUserValidation()
+      {
+         RuleFor(x => x.Username)
+            .NotEmpty()
+            .MinimumLength(3)
+            .MaximumLength(200)
+            .Matches("^[a-zA-Z0-9_]{3,200}$");
+
+         RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(12)
+            .MaximumLength(200)
+            .Matches("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_]).{12,}$");
+      }
+
+   }
+}
