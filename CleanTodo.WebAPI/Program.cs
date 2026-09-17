b981c3c209aaae9254ee374805fffbc043185254
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-17 09:33:58 -0400

    I've deployed on ther server

--- CleanTodo.WebAPI/Program.cs
+++ CleanTodo.WebAPI/Program.cs
@@ -31,7 +31,7 @@ public class Program
             app.UseSwaggerUI();
         }
 
-        app.UseHttpsRedirection();
+      app.UseHttpsRedirection();
         app.UseAuthorization();
         app.MapControllers();
 
