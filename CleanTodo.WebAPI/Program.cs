59ec7a031f83e5da12e30aa7d91197009ca92744
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-20 18:06:35 -0400

    Adding swagger for the api in production

--- CleanTodo.WebAPI/Program.cs
+++ CleanTodo.WebAPI/Program.cs
@@ -5,36 +5,32 @@ using CleanTodo.Infrastructure;
 
 public class Program
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
+   public static void Main(string[] args)
+   {
+      var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-        builder.Services.AddControllers();
+      // Add services to the container.
+      builder.Services.AddControllers();
 
-        // Add Application Layer
-        builder.Services.AddApplication();
+      // Add Application Layer
+      builder.Services.AddApplication();
 
-        // Add Infrastructure Layer
-        builder.Services.AddInfrastructure(builder.Configuration);
+      // Add Infrastructure Layer
+      builder.Services.AddInfrastructure(builder.Configuration);
 
-        // Add Swagger/OpenAPI
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+      // Add Swagger/OpenAPI
+      builder.Services.AddEndpointsApiExplorer();
+      builder.Services.AddSwaggerGen();
 
-        var app = builder.Build();
+      var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+      app.UseSwagger();
+      app.UseSwaggerUI();
 
       app.UseHttpsRedirection();
-        app.UseAuthorization();
-        app.MapControllers();
+      app.UseAuthorization();
+      app.MapControllers();
 
-        app.Run();
-    }
+      app.Run();
+   }
 }
