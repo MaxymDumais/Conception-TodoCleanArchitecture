581627ec1499cdbd1b5f025b4a64e1001687a631
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-20 13:55:56 -0400

    New tests for the auto deployment

--- CleanTodo.WebAPI/Controllers/TodoController.cs
+++ CleanTodo.WebAPI/Controllers/TodoController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 public class TodoController(GetAllTodosUseCase getAllUseCase, GetTodoUseCase getTodoUseCase, CreateTodoUseCase createTodoUseCase) : ControllerBase
 {
    [HttpGet]
-   [Route("getAllTodos")]
+   [Route("getTodos")]
    public async Task<ActionResult<IEnumerable<TodoDto>>> GetAll()
    {
       var todos = await getAllUseCase.Execute();
