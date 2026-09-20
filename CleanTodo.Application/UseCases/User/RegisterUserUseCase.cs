b90f3d64c9d6e8928bfc68118bbca80421bbb507
Maxym Dumais <dumais.maxym@gmail.com>
2026-09-20 17:56:25 -0400

    I forget tu put an await before calling FindByInfo

--- CleanTodo.Application/UseCases/User/RegisterUserUseCase.cs
+++ CleanTodo.Application/UseCases/User/RegisterUserUseCase.cs
@@ -25,7 +25,7 @@ namespace CleanTodo.Application.UseCases.User
          ValidationResult validationResult = await _validator.ValidateAsync(registerUserDto);
          if (!validationResult.IsValid)
             throw new ValidationException(validationResult.Errors);
-         else if (_userRepository.FindByInfo(registerUserDto.Username, registerUserDto.Password) != null)
+         else if (await _userRepository.FindByInfo(registerUserDto.Username, registerUserDto.Password) != null)
             throw new Exception("Erreur lors de la création du compte");
          await _userRepository.Add(new Domain.Entities.User(registerUserDto.Username, registerUserDto.Password));
          return "Compte créé!";
